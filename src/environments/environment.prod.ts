272da8ebb335e1ee82026542f1a882e6f7e6fc66
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-10-14 22:59:05 +0200

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  jsonServerApiUrl: "http://localhost:3000",
+  backendApiUrl: "http://localhost:8080/take",
+  mockApiUrl: false
 };
